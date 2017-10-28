fd32d95a2ac70edbfe5bf48c1d87460ce7aa422c
Nancy Kum <nk0414@gmail.com>
2017-10-28 04:19:31 +0000

    clean-up redundant code and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @article_list = Article.all
   end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def create
@@ -26,7 +27,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)   #need the white-listed article_params
       #show saved title and desc
       flash[:notice] = "Article was successfully updated"
@@ -38,17 +38,19 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    
     def article_params
       params.require(:article).permit(:title, :description)   #require from key :article the values for :title, :description
     end
