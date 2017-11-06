92ef749670d357382a64d9ca6ae05616ddc1df91
Nancy Kum <nk0414@gmail.com>
2017-11-06 02:00:37 +0000

    Correction for show article list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Your account successfully"
+      flash[:success] = "Your account was successfully updated"
       redirect_to articles_path
     else
       render 'edit'
